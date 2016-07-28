39b6ba445cd586a1822dc628763bf1554fb56366
Blaze Sterling <blazekayn@gmail.com>
2016-07-28 14:37:52 -0400

    Added Datbase connection

--- App_Code/Player.cs
+++ App_Code/Player.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +10,11 @@ using System.Web;
 /// </summary>
 public class Player
 {
+    public int PlayerID;
+    public string DisplayName;
+    public int Honor;
+    public int Experience;
+
     public Player()
     {
         //
@@ -22,7 +29,24 @@ public class Player
     /// <param name="PlayerID">The ID of the player to load from the database</param>
     public Player(int PlayerID)
     {
-
+        using (SqlConnection conn = new SqlConnection(ConfigurationManager.AppSettings["DB"].ToString()))
+        {
+            conn.Open();
+            string sql = "SELECT DisplayName, Honor, Experience FROM PLAYER WHERE PlayerID=@PlayerID";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("@PlayerID", PlayerID);
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                DisplayName = dr["DisplayName"].ToString();
+                Honor = dr.GetInt32(dr.GetOrdinal("Honor"));
+                Experience = dr.GetInt32(dr.GetOrdinal("Experience"));
+                this.PlayerID = PlayerID;
+            }
+            dr.Close();
+            conn.Close();
+            conn.Dispose();
+        }
     }
     
 }
