9141bfd23a3bca1a7066a979088a557fdf92a85d
Dylan <Dylan@DESKTOP-Q3GCVQ8>
2016-07-30 15:25:42 -0400

    Added Global Functions Class and getbuildtime function

---
+++ App_Code/Global_Functions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Global_Functions
+/// </summary>
+public class Global_Functions
+{
+    public Global_Functions()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+
+    }
+         /// <summary>
+        /// Calculates Build Time for a building
+        /// </summary>
+        /// <param name="CurrentBuildingLevel"></param>
+        /// <param name="BaseBuildTime"></param>
+        /// <returns></returns>
+        public Double GetBuildTime(int CurrentBuildingLevel, int BaseBuildTime)
+    {
+        Double Time = (Math.Pow(.005, BaseBuildTime) + CurrentBuildingLevel) * (CurrentBuildingLevel * Math.Pow(1.35, CurrentBuildingLevel));
+
+        return Time;
+    }
+
+}
